a98e0b7cde2f1b293be9e7d1b027238b6cdb78dc
Raul <raulrufino0@gmail.com>
2021-10-27 09:17:32 -0300

    Adiciona estrutura basica

---
+++ logger.py
@@ -0,0 +1,9 @@
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %(funcName)s: %(message)s',
+    datefmt='%d-%m-%Y %H:%M:%S',
+)
+
+logger = logging.getLogger(__name__)
